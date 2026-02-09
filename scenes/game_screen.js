40cec0501a206d9eeb04c577e9a190ee5e5cdd2b
Emerson-Fleming <85723483+Emerson-Fleming@users.noreply.github.com>
2026-02-08 22:09:12 -0800

    need a bit more time

--- scenes/game_screen.js
+++ scenes/game_screen.js
@@ -2,10 +2,17 @@ class game_screen {
     constructor() {
         let i = 0;
         let t = 2000; // 2 seconds per image
+        let plantedTrees = []; // array of planted tree objects: {img, x, y}
+        let annotations = null; // store loaded annotations
 
         this.buttons = [];
 
         this.setup = function () {
+            // Load annotations once at setup
+            loadJSON('assets/json/annotations.json', (data) => {
+                annotations = data.annotations;
+                print('Annotations loaded:', annotations);
+            });
             this.updateImage();
         }
 
@@ -20,21 +27,37 @@ class game_screen {
                     height / streetImages[i].height
                 );
 
+                let scaledWidth = streetImages[i].width * scale;
+                let scaledHeight = streetImages[i].height * scale;
+
+                // Draw street image
                 image(
                     streetImages[i],
                     0,
                     0,
-                    streetImages[i].width * scale,
-                    streetImages[i].height * scale
+                    scaledWidth,
+                    scaledHeight
                 );
 
                 filter(GRAY);
 
-                this.drawTreeButtons();
+                // Draw planted trees on top (scaled to match the image)
+                for (let tree of plantedTrees) {
+                    imageMode(CENTER);
+                    // Scale tree position relative to the scaled image dimensions
+                    let treeX = tree.x * scale;
+                    let treeY = tree.y * scale;
+                    // Scale tree image size as well
+                    let treeScale = scale * 0.3; // Adjust 0.3 to make trees bigger/smaller
+                    image(tree.img, treeX, treeY, tree.img.width * treeScale, tree.img.height * treeScale);
+                }
+
+                this.drawTreeButtons(scale, scaledWidth, scaledHeight);
             }
         }
 
         this.updateImage = function () {
+            plantedTrees = []; // reset planted trees for new image
             if (i < streetImages.length - 1) {
                 i++;
                 setTimeout(() => this.updateImage(), t);
@@ -44,15 +67,15 @@ class game_screen {
             }
         }
 
-        this.drawTreeButtons = function () {
-            // build buttons once per image
+        this.drawTreeButtons = function (scale, scaledWidth, scaledHeight) {
+            // Build buttons with scaled positions
             this.buttons = [
-                { img: carnaubaBtn, tree: carnaubaImg, x: 1 * (streetImages[i].width / 8) - 200, y: streetImages[i].height - 200 },
-                { img: cajueiroBtn, tree: cajueiroImg, x: 2 * (streetImages[i].width / 8) - 200, y: streetImages[i].height - 200 },
-                { img: juazeiroBtn, tree: juazeiroImg, x: 3 * (streetImages[i].width / 8) - 200, y: streetImages[i].height - 200 },
-                { img: jucaBtn, tree: jucaImg, x: 4 * (streetImages[i].width / 8) - 200, y: streetImages[i].height - 200 },
-                { img: mororoBtn, tree: mororoImg, x: 5 * (streetImages[i].width / 8) - 200, y: streetImages[i].height - 200 },
-                { img: oitiBtn, tree: oitiImg, x: 6 * (streetImages[i].width / 8) - 200, y: streetImages[i].height - 200 }
+                { img: carnaubaBtn, tree: carnaubaImg, x: 1 * (scaledWidth / 8) - 200, y: scaledHeight - 200 },
+                { img: cajueiroBtn, tree: cajueiroImg, x: 2 * (scaledWidth / 8) - 200, y: scaledHeight - 200 },
+                { img: juazeiroBtn, tree: juazeiroImg, x: 3 * (scaledWidth / 8) - 200, y: scaledHeight - 200 },
+                { img: jucaBtn, tree: jucaImg, x: 4 * (scaledWidth / 8) - 200, y: scaledHeight - 200 },
+                { img: mororoBtn, tree: mororoImg, x: 5 * (scaledWidth / 8) - 200, y: scaledHeight - 200 },
+                { img: oitiBtn, tree: oitiImg, x: 6 * (scaledWidth / 8) - 200, y: scaledHeight - 200 }
             ];
 
             for (let b of this.buttons) {
@@ -68,30 +91,44 @@ class game_screen {
                     mouseY > b.y &&
                     mouseY < b.y + b.img.height
                 ) {
-                    this.plantTree(b.tree);
+                    this.plantTree(b);
                 }
             }
         }
 
-        this.plantTree = function (treeType) {
-            loadJSON('assets/json/annotations.json', (data) => {
-                let annotations = data.annotations;
-                print(annotations);
-                let points = annotations[streetImageNames[i]]; // Use streetImageNames to get the correct key
-                print(points);
+        this.plantTree = function (button) {
+            if (!annotations) {
+                print('Annotations not loaded yet');
+                return;
+            }
 
-                if (points && points.length > 0) {
-                    // find point with lowest Y value
-                    let lowestPoint = points.reduce(
-                        (lowest, p) => (p.y > lowest.y ? p : lowest),
-                        points[0]
-                    );
+            let points = annotations[streetImageNames[i]]; // Use streetImageNames to get the correct key
+            print('Points for', streetImageNames[i], ':', points);
 
-                    image(treeType, lowestPoint.x, lowestPoint.y);
-                    print(treeType)
-                    print(lowestPoint);
+            if (points && points.length > 0) {
+                // Find the next available point (not already planted)
+                let availablePoints = points.filter((p, idx) => !plantedTrees.some(t => t.pointIndex === idx));
+                
+                if (availablePoints.length === 0) {
+                    print('All planting points used for this image');
+                    return;
                 }
-            });
+
+                // Get the first available point (or use lowest Y value logic)
+                let selectedPoint = availablePoints[0];
+                
+                // Store the planted tree with original coordinates (will be scaled in draw)
+                plantedTrees.push({
+                    img: button.tree,
+                    x: selectedPoint.x,
+                    y: selectedPoint.y,
+                    pointIndex: points.indexOf(selectedPoint)
+                });
+                
+                print('Planted tree at:', selectedPoint);
+            } else {
+                print('No planting points found for this image');
+            }
         }
     }
 }
