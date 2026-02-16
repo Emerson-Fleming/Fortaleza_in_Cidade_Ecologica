23087aec6d10348922a65404ab20d9a8d83e7b4f
Emerson-Fleming <85723483+Emerson-Fleming@users.noreply.github.com>
2026-02-15 21:05:34 -0800

    menu select

--- scenes/info_screen.js
+++ scenes/info_screen.js
@@ -1,14 +1,26 @@
 class info_screen {
-    constructor() { 
+    constructor() {
         let backgroundImg;
         let assetPath = 'assets/';
+        let font;
+        let selectedOption = 0; // 0: INTRODUCING OUR STARS, 1: HOW TO PLAY, 2: START GAME
+        let buttons = [];
+
         this.setup = function () {
             this.preload();
-            textFont('Press Start 2P');
+            textFont(font);
+
+            // Initialize button positions
+            buttons = [
+                { label: 'INTRODUCING\nOUR STARS', x: width / 8, y: height / 3, index: 0 },
+                { label: 'HOW TO PLAY', x: width / 8, y: height / 2, index: 1 },
+                { label: 'START GAME', x: width / 8, y: height * 2 / 3, index: 2 }
+            ];
         }
 
         this.preload = function () {
             backgroundImg = loadImage(assetPath + 'background.png');
+            font = loadFont(assetPath + 'fonts/PressStart2P.ttf');
         }
 
         this.draw = function () {
@@ -17,13 +29,40 @@ class info_screen {
         }
 
         this.drawOptionsMenu = function () {
-            text('INTRODUCING OUR STARS')
-            text('HOW TO PLAY')
-            text('START GAME')
+            textFont(font);
+            textSize(24);
+
+            for (let btn of buttons) {
+                if (btn.index === selectedOption) {
+                    fill(0, 176, 0); // #00b000 when selected
+                } else {
+                    fill(255); // white when not selected
+                }
+                text(btn.label, btn.x, btn.y);
+            }
+        }
+
+        this.mouseClicked = function () {
+            for (let btn of buttons) {
+                // Get text bounds for hit detection
+                textFont(font);
+                textSize(24);
+
+                let btnTextWidth = textWidth(btn.label);
+                let textHeight = 24 * 3; // approximate height for multiline text
+
+                if (mouseX > btn.x &&
+                    mouseX < btn.x + btnTextWidth &&
+                    mouseY > btn.y - textHeight / 2 &&
+                    mouseY < btn.y + textHeight / 2) {
+                    selectedOption = btn.index;
+                    break;
+                }
+            }
         }
 
         this.drawOurStars = function () {
-            
+
         }
 
         this.drawHowToPlay = function () {
