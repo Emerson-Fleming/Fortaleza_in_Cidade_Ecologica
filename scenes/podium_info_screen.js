d8f1cf38d5b6f7d0c7e892f2d3d2fbac92e3c8a6
Emerson Fleming <85723483+Emerson-Fleming@users.noreply.github.com>
2026-03-18 23:34:41 -0700

    okay almost there

--- scenes/podium_info_screen.js
+++ scenes/podium_info_screen.js
@@ -1,13 +1,38 @@
 class podium_info_screen {
     constructor() {
         let data;
+        let treeData;
         let ignoreClicksUntil = 0;
+
+        // Helper function for word-wrapping text
+        const wrapText = function(txt, x, y, maxWidth, lineHeight) {
+            let words = txt.split(' ');
+            let line = '';
+            let yPos = y;
+            
+            for (let i = 0; i < words.length; i++) {
+                let testLine = line + words[i] + ' ';
+                let testWidth = textWidth(testLine);
+                
+                if (testWidth > maxWidth && i > 0) {
+                    text(line, x, yPos);
+                    line = words[i] + ' ';
+                    yPos += lineHeight;
+                } else {
+                    line = testLine;
+                }
+            }
+            text(line, x, yPos);
+        };
+
         this.setup = function () {
             textFont(font);
         }
 
         this.enter = function () {
             data = this.sceneArgs;
+            // Look up full tree data from global trees array
+            treeData = data ? trees.find(t => t.type === data.type) : null;
             ignoreClicksUntil = Date.now() + 250;
         }
 
@@ -15,18 +40,54 @@ class podium_info_screen {
             imageMode(CORNER);
             background(backgroundImg);
 
-            if (!data) {
+            if (!data || !treeData) {
                 return;
             }
 
-            imageMode(CENTER)
-            image(data.tree_img, width / 4, height / 2 - 100);
+            push();
 
-            fill(255);
-            textSize(24);
+            // Layout dimensions
+            let boxW = width * 0.8;
+            let boxH = height * 0.7;
+            let boxX = (width - boxW) / 2;
+            let boxY = (height - boxH) / 2;
+
+            // Left side: tree image
+            let imgSize = boxH * 0.8;
+            let imgX = boxX;
+            let imgY = boxY + (boxH - imgSize) / 2;
+            imageMode(CORNER);
+            image(treeData.img(), imgX, imgY, imgSize, imgSize);
+
+            // Right side: title and description
+            let textX = boxX + imgSize + 60;
+            let textW = boxW - imgSize - 60;
+
             textFont(font);
-            textAlign(CENTER);
-            text(`${data.type} x${data.count}`, 3 *width / 4, height / 2);
+            textAlign(LEFT, TOP);
+
+            // Title
+            fill(255);
+            textSize(36);
+            text(treeData.type, textX, imgY + 20);
+
+            // Count subtitle
+            fill(0, 176, 0);
+            textSize(24);
+            text(`Planted: ${data.count}`, textX, imgY + 80);
+
+            // Description
+            fill(255);
+            textSize(18);
+            wrapText(treeData.desc(), textX, imgY + 140, textW, 40);
+
+            // Back hint
+            textSize(14);
+            fill(180);
+            textAlign(CENTER, BOTTOM);
+            text('click anywhere to return to title', width / 2, height - 40);
+
+            pop();
         }
 
         this.mouseClicked = function () {
